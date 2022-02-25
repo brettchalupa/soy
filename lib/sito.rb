b9f44dc37aedabed5bda2d6af9baec5faa36835d
Brett Chalupa <brettchalupa@gmail.com>
2022-02-25 14:55:28 -0500

    Initialize new gem

---
+++ lib/sito.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sito/version"
+
+module Sito
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sito/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Sito
+  VERSION = "0.1.0"
+end
