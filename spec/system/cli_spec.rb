3421719fd429ba729a864411c2a62a2650a7fc1d
Brett Chalupa <brettchalupa@gmail.com>
2022-02-25 21:56:37 -0500

    DRY up CLI spec

--- spec/system/cli_spec.rb
+++ spec/system/cli_spec.rb
@@ -3,22 +3,26 @@
 require "spec_helper"
 
 RSpec.describe "CLI" do
+  def run_cmd(cmd)
+    `ruby -Ilib ./exe/sito #{cmd}`
+  end
+
   describe "sito bogus" do
     it "outputs an error w/ help recommendation" do
-      output = `ruby -Ilib ./exe/sito bogus`
+      output = run_cmd("bogus")
       expect(output).to match(/`bogus` is not a command/)
       expect(output).to match(/Run `sito help` for list of commands/)
     end
   end
 
   describe "sito version" do
+    let(:output) { run_cmd("version") }
+
     it "outputs the version" do
-      output = `ruby -Ilib ./exe/sito version`
       expect(output).to match(/#{Sito::VERSION}/)
     end
 
     it "supports the v alias" do
-      output = `ruby -Ilib ./exe/sito v`
       expect(output).to match(/#{Sito::VERSION}/)
     end
   end
