b72110380360e60deaee5ad6bf4b74652c7342f6
Brett Chalupa <brettchalupa@gmail.com>
2022-03-05 08:46:20 -0500

    Move `soy new` system spec into its own file
    & fleshes it out

---
+++ spec/system/cli/new_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "soy new" do
+  def clean_up(dir)
+    FileUtils.rm_rf(dir)
+  end
+
+  let(:site_name) { "new_site" }
+
+  before { clean_up(site_name) }
+
+  after { clean_up(site_name) }
+
+  it "generates a new site from the template" do
+    output = run_cmd("new #{site_name}")
+
+    expect(output).to match(/New Soy site created, view in: new_site/)
+
+    [".gitignore", "content/index.html.erb", "views/layout.html.erb"].each do |file|
+      expect(File.read("new_site/#{file}")).to eql(File.read("lib/soy/template/#{file}"))
+    end
+
+    expect(File.read("new_site/.gitignore")).to match(/build/)
+  end
+end
